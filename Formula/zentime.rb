f6ab4b6ec7395d236839eb7b37bdc11b6a000614
Theo Salzmann <dev@on3iro.de>
2022-12-09 22:41:41 +0100

    zentime 0.6.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/zentime.rb
+++ Formula/zentime.rb
@@ -4,8 +4,8 @@
 class Zentime < Formula
   desc "Pomodoro and productivity timer written in rust"
   homepage "https://github.com/on3iro/zentime"
-  url "https://github.com/on3iro/zentime-rs/releases/download/0.5.2/zentime.tar.gz"
-  sha256 "35269ca91f33d54ceb3b8a258bd8826b8667a0e6057b3a5e54cc050d49a196b8"
+  url "https://github.com/on3iro/zentime-rs/releases/download/0.6.0/zentime.tar.gz"
+  sha256 "9b84c4229bc5ef8c2f557a591ca9a2757c00faf05e692c1ecd7479d5e1d1c9d8"
   license "MIT"
 
   def install
